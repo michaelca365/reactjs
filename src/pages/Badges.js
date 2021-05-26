3c598b4a70befb0f7fe0727968aec34cb9b24746
Michael Camelo <michaelca365@gmail.com>
2021-05-26 16:24:11 -0500

    Pagina actualizada

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,48 +1,65 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import BadgesList from "./BadgesList";
 import confLogo from "../images/badge-header.svg";
 import "./styles/Badges.css";
+import { Link } from "react-router-dom";
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-        irstName: "Freda",
-        lastName: "Grady",
-        email: "Leann_Berge@gmail.com",
-        jobTitle: "Legacy Brand Director",
-        twitter: "FredaGrady22221-7573",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon",
-      },
-      {
-        id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-        firstName: "Major",
-        lastName: "Rodriguez",
-        email: "Ilene66@hotmail.com",
-        jobTitle: "Human Research Architect",
-        twitter: "ajorRodriguez61545",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon",
-      },
-      {
-        id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-        firstName: "Daphney",
-        lastName: "Torphy",
-        email: "Ron61@hotmail.com",
-        jobTitle: "National Markets Officer",
-        twitter: "DaphneyTorphy96105",
-        avatarUrl:
-          "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon",
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    console.log("1. Contructor()");
+    this.state = {
+      data: [],
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+            irstName: "Freda",
+            lastName: "Grady",
+            email: "Leann_Berge@gmail.com",
+            jobTitle: "Legacy Brand Director",
+            twitter: "FredaGrady22221-7573",
+            avatarUrl:
+              "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon",
+          },
+          {
+            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+            firstName: "Major",
+            lastName: "Rodriguez",
+            email: "Ilene66@hotmail.com",
+            jobTitle: "Human Research Architect",
+            twitter: "ajorRodriguez61545",
+            avatarUrl:
+              "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon",
+          },
+          {
+            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+            firstName: "Daphney",
+            lastName: "Torphy",
+            email: "Ron61@hotmail.com",
+            jobTitle: "National Markets Officer",
+            twitter: "DaphneyTorphy96105",
+            avatarUrl:
+              "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon",
+          },
+        ],
+      });
+    }, 3000);
+    console.log("3. ComponentDidMount");
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("5. componentDidUpdate");
+    console.log({ prevProps, prevState });
+    console.log({ props: this.props, state: this.state });
+  }
   render() {
+    console.log("2. Render()");
     return (
-      <div>
-        <Navbar />
+      <React.Fragment>
         <div className="Badges">
           <div className="Badges__hero">
             <div className="Badges__container">
@@ -57,18 +74,17 @@ class Badges extends React.Component {
 
         <div className="Badge__container">
           <div className="Badges__buttons">
-            <a href="/badges/new" className="btn btn-primary">
+            <Link to="/badges/new" className="btn btn-primary">
               New Badge
-            </a>
+            </Link>
           </div>
-
           <div className="Badges__list">
             <div className="Badges__container">
               <BadgesList badges={this.state.data} />
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/pages/BadgesList.js
+++ src/pages/BadgesList.js
@@ -1,15 +1,32 @@
 import React from "react";
-
+import twitterLogo from "../images/gorjeo.svg";
+import "./styles/BadgeList.css";
 class BadgesList extends React.Component {
   render() {
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+            <li className="listStyle" key={badge.id}>
+              <div>
+                <div className="avatar">
+                  <span>
+                    <img src={badge.avatarUrl} alt="user avatar" />
+                  </span>
+                </div>
+                <div className="userContent">
+                  <h5>
+                    {badge.firstName} {badge.lastName}
+                  </h5>
+                  <div className="userTwitter">
+                    <span>
+                      <img src={twitterLogo} alt="TwitterLogo" />
+                    </span>
+                    <p>@{badge.twitter}</p>
+                  </div>
+                  <p>{badge.jobTitle}</p>
+                </div>
+              </div>
             </li>
           );
         })}
