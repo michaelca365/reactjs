1439a64829d6e947610e17541ed5ce7e213460f6
Steve Guevara <michaelito3d@gmail.com>
2021-05-26 19:28:52 -0500

    pruebas

--- src/pages/BadgesList.js
+++ src/pages/BadgesList.js
@@ -1,15 +1,30 @@
 import React from "react";
-
+import './styles/BadgeList.css';
+import twitterLogo from '../images/gorjeo.svg';
 class BadgesList extends React.Component {
   render() {
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+            <li className="listStyle" key={badge.id}>
+              <div>
+                <div className="avatar">
+                  <span>
+                    <img alt="Avatar Logo" src={badge.avatarUrl} />
+                  </span>
+                </div>
+                <div className="userContent">
+                  <h5>{badge.firstName} {badge.lastName}</h5>
+                  <div class="userTwitter">
+                    <span>
+                      <img src= {twitterLogo} alt="Twitter logo" />
+                    </span>
+                    <p>@{badge.twitter}</p>
+                  </div>
+                  <p> {badge.jobTitle} </p>
+                </div>
+              </div>
             </li>
           );
         })}
